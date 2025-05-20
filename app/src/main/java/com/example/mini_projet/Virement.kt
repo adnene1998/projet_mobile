15f30b005d4ca4c91967a44c09bcf9002ea53edd
Adnene halila <adnenehalila@gmail.com>
2025-05-20 16:48:34 +0200

    20 s

--- app/src/main/java/com/example/mini_projet/Virement.kt
+++ app/src/main/java/com/example/mini_projet/Virement.kt
@@ -1,20 +1,54 @@
 package com.example.mini_projet
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mini_projet.databinding.ActivityMainBinding
+import com.example.mini_projet.databinding.ActivityVirementBinding
 
 class Virement : AppCompatActivity() {
+    val binding = ActivityVirementBinding.inflate(layoutInflater)
+
+    private val accueilLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            // Traiter le résultat
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_virement)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.btnVirement.setOnClickListener {
+            val montantText = binding.montant.text.toString()
+            val ribText = binding.rib.text.toString()
+
+            if (montantText.isNotEmpty() && ribText.isNotEmpty()) {
+
+                Toast.makeText(
+                    this,
+                    "Virement de $montantText vers RIB $ribText effectué",
+                    Toast.LENGTH_SHORT
+                ).show()
+                val intent = Intent(this, Accueil::class.java)
+                accueilLauncher.launch(intent)
+
+            } else {
+                Toast.makeText(this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
